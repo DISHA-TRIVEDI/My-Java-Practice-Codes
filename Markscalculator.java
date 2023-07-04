50a7991050b7be2dee989cb797b4a8efdff74c93
DISHA TRIVEDI <107933059+DISHA-TRIVEDI@users.noreply.github.com>
2023-07-05 02:00:03 +0530

    Markscalculator.java

--- Markscalculator.java
+++ Markscalculator.java
@@ -1,13 +1,5 @@
+/* -------------------THIS IS THE CODE FOR IN-BUILT VALUES-------------------------------
 public class Markscalculator {
-    /*public static void main(String[] args) {
-        int arr[]={94, 88, 92, 90, 89};
-        for(int i=0;i<arr.length;i++){
-           int sum= arr[i]+arr[i+1];
-           i++;
-           float div= sum/300;
-           double percent= div*100;
-            System.out.println("The percentage of student is: "+ percent+ "%");
-        }*/
     public static void main(String[] args) {  
         int a=96;
         int b=88;
@@ -19,4 +11,25 @@ public class Markscalculator {
         double percent= div*100d;
         System.out.println("The percentage of student is: "+ percent + "%");
     }
+}*/
+
+//----------------------THIS IS THE CODE FOR VALUES FROM USER-------------------------
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+		System.out.print("Enter 1st subject mark: ");
+		int mark= sc.nextInt();
+			System.out.print("Enter 2nd subject mark: ");
+		int mark2= sc.nextInt();
+			System.out.print("Enter 3rd subject mark: ");
+		int mark3= sc.nextInt();
+			System.out.print("Enter 4th subject mark: ");
+		int mark4= sc.nextInt();
+			System.out.print("Enter 5th subject mark: ");
+		int mark5= sc.nextInt();
+		double percent= (mark+mark2+mark3+mark4+mark5)*100/500d;
+		System.out.println("Percentage of Marks is: "+percent+"%");
+	}
 }
